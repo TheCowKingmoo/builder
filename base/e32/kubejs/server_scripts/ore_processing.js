5c53e4909390f4b6cd47186c71507f4459dd4e07
Cow <wrought.metal@gmail.com>
2023-10-28 12:36:39 -0700

    add plates and add zinc raw ore

--- base/e32/kubejs/server_scripts/ore_processing.js
+++ base/e32/kubejs/server_scripts/ore_processing.js
@@ -11,5 +11,12 @@ ServerEvents.recipes(event => {
     // only ore that doesnt follow this format
     event.recipes.modern_industrialization.macerator(2, 200)
         .itemIn("1x #c:zinc_ores")
-        .itemOut("3x #c:raw_zinc_ores");    
+        .itemOut("3x #c:raw_zinc_ores");
+
+    // MI does not have a crushed zinc variant
+    event.recipes.modern_industrialization.macerator(2, 200)
+        .itemIn("1x #c:raw_zinc_ores")
+        .itemOut("create:crushed_raw_zinc", 1.0)
+        .itemOut("create:crushed_raw_zinc", 0.5);   //why i cant just use 1.5 is beyond me...
+
 })
---
+++ base/e32/kubejs/server_scripts/plates.js
@@ -0,0 +1,21 @@
+ServerEvents.recipes(e => {
+
+    e.recipes.create.pressing('#c:steel_plates', '#c:steel_ingots');
+    e.recipes.create.pressing('#c:diamond_plates', '#c:diamonds');
+    e.recipes.create.pressing('#c:electrum_plates', '#c:electrum_ingots');
+    e.recipes.create.pressing('#c:emerald_plates', '#c:emeralds');
+    e.recipes.create.pressing('#c:invar_plates', '#c:invar_ingots');
+    e.recipes.create.pressing('#c:kanthal_plates', '#c:kanthal_ingots');
+    e.recipes.create.pressing('#c:lapis_plates', '#c:lapis');
+    e.recipes.create.pressing('#c:lead_plates', '#c:lead_ingots');
+    e.recipes.create.pressing('#c:nickel_plates', '#c:nickel_ingots');
+    e.recipes.create.pressing('#c:platinum_plates', '#c:platinum_ingots');
+    e.recipes.create.pressing('#c:silicon_plates', '#c:silicon_ingots');
+    e.recipes.create.pressing('#c:silver_plates', '#c:silver_ingots');
+    e.recipes.create.pressing('#c:stainless_steel_plates', '#c:stainless_steel_ingots');
+    e.recipes.create.pressing('#c:superconductor_plates', '#c:superconductor_ingots');
+    e.recipes.create.pressing('#c:tin_plates', '#c:tin_ingots');
+    e.recipes.create.pressing('#c:titanium_plates', '#c:titanium_ingots');
+    e.recipes.create.pressing('#c:tungsten_plates', '#c:tungsten_ingots');
+
+  })
